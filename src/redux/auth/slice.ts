445822ca74cc75b6c43ec0369eb36fe543600fb9
Yuriy <>
2025-01-26 18:59:25 +0200

    Update slice.ts

--- src/redux/auth/slice.ts
+++ src/redux/auth/slice.ts
@@ -1,9 +1,11 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { registerUser } from './operations';
 
 // Типізація стану
 interface AuthState {
   user: User | null;
   token: string | null;
+  error: string | null; // Додаємо стан для помилок
 }
 
 interface User {
@@ -16,6 +18,7 @@ interface User {
 const initialState: AuthState = {
   user: null,
   token: null,
+  error: null, // ініціалізуємо як null
 };
 
 const authSlice = createSlice({
@@ -30,6 +33,7 @@ const authSlice = createSlice({
       state.user = action.payload.user;
       state.token = action.payload.token;
       localStorage.setItem('token', action.payload.token);
+      state.error = null; // Очищаємо помилку при успішному вході
     },
     // Логаут
     logout: state => {
@@ -37,8 +41,16 @@ const authSlice = createSlice({
       state.token = null;
       localStorage.removeItem('token');
     },
+    setError: (state, action: PayloadAction<string>) => {
+      state.error = action.payload; // Встановлюємо помилку
+    },
+  },
+  extraReducers: builder => {
+    builder.addCase(registerUser.rejected, (state, action) => {
+      state.error = action.payload || 'An error occurred during registration.'; // обробка помилки
+    });
   },
 });
 
-export const { loginSuccess, logout } = authSlice.actions;
+export const { loginSuccess, logout, setError } = authSlice.actions;
 export default authSlice.reducer;
