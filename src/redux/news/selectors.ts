e1b9f8a537861d526b3aadde2eaf205d40b66ca3
Yuriy <>
2025-01-26 22:37:56 +0200

    Update selectors.ts

---
+++ src/redux/news/selectors.ts
@@ -0,0 +1,6 @@
+import { RootState } from '../store';
+
+export const selectNewsList = (state: RootState) => state.news.newsList;
+export const selectSearchQuery = (state: RootState) => state.news.searchQuery;
+export const selectCurrentPage = (state: RootState) => state.news.currentPage;
+export const selectTotalPages = (state: RootState) => state.news.totalPages;
