0959064e8e8d7c5b1a32e76c5ddba9a5adcbf705
Yuriy <>
2025-01-26 20:07:37 +0200

    Update slice.ts

--- src/redux/auth/slice.ts
+++ src/redux/auth/slice.ts
@@ -1,11 +1,10 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { registerUser } from './operations';
+import { registerUser, loginUser } from './operations';
 
-// Типізація стану
 interface AuthState {
   user: User | null;
   token: string | null;
-  error: string | null; // Додаємо стан для помилок
+  error: string | null;
 }
 
 interface User {
@@ -14,18 +13,16 @@ interface User {
   email: string;
 }
 
-// Початковий стан
 const initialState: AuthState = {
   user: null,
   token: null,
-  error: null, // ініціалізуємо як null
+  error: null,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    // Логін успішний
     loginSuccess: (
       state,
       action: PayloadAction<{ user: User; token: string }>
@@ -33,24 +30,27 @@ const authSlice = createSlice({
       state.user = action.payload.user;
       state.token = action.payload.token;
       localStorage.setItem('token', action.payload.token);
-      state.error = null; // Очищаємо помилку при успішному вході
+      state.error = null;
     },
-    // Логаут
     logout: state => {
       state.user = null;
       state.token = null;
       localStorage.removeItem('token');
     },
     setError: (state, action: PayloadAction<string>) => {
-      state.error = action.payload; // Встановлюємо помилку
+      state.error = action.payload;
     },
   },
   extraReducers: builder => {
-    builder.addCase(registerUser.rejected, (state, action) => {
-      state.error = action.payload || 'An error occurred during registration.'; // обробка помилки
-    });
+    builder
+      .addCase(registerUser.rejected, (state, action) => {
+        state.error =
+          action.payload || 'An error occurred during registration.';
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.error = action.payload || 'An error occurred during login.';
+      });
   },
 });
-
 export const { loginSuccess, logout, setError } = authSlice.actions;
 export default authSlice.reducer;
