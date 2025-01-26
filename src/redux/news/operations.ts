c475fce55403f2b3f9179730220e0fd98b706d54
Yuriy <>
2025-01-26 22:31:35 +0200

    Update operations.ts

--- src/redux/news/operations.ts
+++ src/redux/news/operations.ts
@@ -1,18 +1,29 @@
+import axios from 'axios';
 import { AppDispatch } from '../store';
-import { setNewsList, setCurrentPage } from './slice';
+import { setNewsList, setCurrentPage, setTotalPages } from './slice';
 
-export const fetchNews = (page: number = 1, searchQuery: string = '') => async (dispatch: AppDispatch) => {
-  try {
-    const response = await fetch(`/api/news?page=${page}&query=${searchQuery}`);
-    const data = await response.json();
+export const fetchNews =
+  (page: number = 1, keyword: string = '', limit: number = 6) =>
+  async (dispatch: AppDispatch) => {
+    try {
+      const response = await axios.get('/news', {
+        params: {
+          page,
+          keyword,
+          limit,
+        },
+      });
 
-    if (response.ok) {
-      dispatch(setNewsList(data.news));
-      dispatch(setCurrentPage(page));
-    } else {
-      console.error('Error fetching news:', data.message);
+      if (response.status === 200) {
+        const { results, totalPages } = response.data;
+
+        dispatch(setNewsList(results));
+        dispatch(setCurrentPage(page));
+        dispatch(setTotalPages(totalPages));
+      } else {
+        console.error('Unexpected response status:', response.status);
+      }
+    } catch (error) {
+      console.error('Error fetching news:', error);
     }
-  } catch (error) {
-    console.error('Error fetching news:', error);
-  }
-};
+  };
