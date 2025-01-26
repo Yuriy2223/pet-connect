99d3a35e4bad728dca47c2948cbd3e173982a3ec
Yuriy <>
2025-01-26 19:23:54 +0200

    Update Api.ts

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -2,32 +2,43 @@ import axios from 'axios';
 
 const API_URL = 'https://petlove.b.goit.study';
 
-const instance = axios.create({
+// Створення екземпляра axios
+export const instance = axios.create({
   baseURL: API_URL,
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
 });
 
-// export const setToken = token => {
-//   instance.defaults.headers.common.Authorization = `Bearer ${token}`;
-// };
+// Функція для встановлення токена
+export const setToken = (token: string) => {
+  instance.defaults.headers.common.Authorization = `Bearer ${token}`;
+  localStorage.setItem('token', token);
+};
 
-// export const clearToken = () => {
-//   delete instance.defaults.headers.common.Authorization;
-// };
+// Функція для очищення токена
+export const clearToken = () => {
+  delete instance.defaults.headers.common.Authorization;
+  localStorage.removeItem('token');
+};
 
-// instance.interceptors.request.use(
-//   config => {
-//     const token = localStorage.getItem('token');
+// Інтерсептор для додавання токена
+instance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
 
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   error => {
-//     return Promise.reject(error);
-//   }
-// );
+// Інтерсептор для обробки помилок
+instance.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401) {
+      clearToken();
+      window.location.href = '/login'; // Перенаправлення при помилці авторизації
+    }
+    return Promise.reject(error);
+  }
+);
