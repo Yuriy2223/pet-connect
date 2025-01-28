c92c1e2487822a6fb4fad54c2edd24d98f47b69d
Yuriy <>
2025-01-28 18:24:34 +0200

    Update DesktopMenu.tsx

--- src/components/Header/DesktopMenu.tsx
+++ src/components/Header/DesktopMenu.tsx
@@ -1,38 +1,3 @@
-// import React from 'react';
-// import { Nav } from '../Nav/Nav';
-// import { AuthNav } from '../AuthNav/AuthNav';
-// import { UserNav } from '../UserNav/UserNav';
-// import { DesktopMenuContainer } from './header.styled';
-
-// interface DesktopMenuProps {
-//   isAuthenticated: boolean;
-//   userName: string;
-//   userAvatar: string;
-//   onLogout: () => void;
-// }
-
-// export const DesktopMenu: React.FC<DesktopMenuProps> = ({
-//   isAuthenticated,
-//   userName,
-//   userAvatar,
-//   onLogout,
-// }) => {
-//   return (
-//     <DesktopMenuContainer>
-//       <Nav />
-//       {isAuthenticated ? (
-//         <AuthNav />
-//       ) : (
-//         <UserNav
-//           userName={userName}
-//           userAvatar={userAvatar}
-//           onLogout={onLogout}
-//         />
-//       )}
-//     </DesktopMenuContainer>
-//   );
-// };
-
 import React from 'react';
 import { Nav } from '../Nav/Nav';
 import { UserNav } from '../UserNav/UserNav';
