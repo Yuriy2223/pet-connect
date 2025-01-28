b4ed36d3dbab57bd7a0c3e44f2d4acd44feb4bcb
Yuriy <>
2025-01-28 18:26:11 +0200

    Update MobileMenu.tsx

--- src/components/Header/MobileMenu.tsx
+++ src/components/Header/MobileMenu.tsx
@@ -55,7 +55,6 @@ export const MobileMenu: React.FC<MobileMenuProps> = ({
         <MenuWrapper>
           <Nav closeMenu={closeMenu} />
           {isAuthenticated ? (
-            // Виправлено: Використовується UserNav для авторизованого користувача
             <UserNav
               closeMenu={closeMenu}
               userName={userName}
@@ -63,7 +62,6 @@ export const MobileMenu: React.FC<MobileMenuProps> = ({
               onLogout={onLogout}
             />
           ) : (
-            // Виправлено: AuthNav відображається для гостей
             <AuthNav closeMenu={closeMenu} />
           )}
         </MenuWrapper>
@@ -71,81 +69,3 @@ export const MobileMenu: React.FC<MobileMenuProps> = ({
     </>
   );
 };
-
-/****************************************** */
-
-// import React, { useEffect } from 'react';
-// import { Nav } from '../Nav/Nav';
-// import { AuthNav } from '../AuthNav/AuthNav';
-// import { UserNav } from '../UserNav/UserNav';
-// import {
-//   CloseButton,
-//   CloseIcon,
-//   MenuWrapper,
-//   MobileMenuContainer,
-//   Overlay,
-// } from './header.styled';
-
-// interface MobileMenuProps {
-//   isOpen: boolean;
-//   closeMenu: () => void;
-//   isAuthenticated: boolean;
-//   userName: string;
-//   userAvatar: string;
-//   onLogout: () => void;
-// }
-
-// export const MobileMenu: React.FC<MobileMenuProps> = ({
-//   isOpen,
-//   closeMenu,
-//   isAuthenticated,
-//   userName,
-//   userAvatar,
-//   onLogout,
-// }) => {
-//   useEffect(() => {
-//     // Функція для закриття меню при натисканні клавіші Escape
-//     const handleKeyDown = (event: KeyboardEvent) => {
-//       if (event.key === 'Escape') {
-//         closeMenu();
-//       }
-//     };
-
-//     // Додаємо обробник події при монтуванні
-//     if (isOpen) {
-//       document.addEventListener('keydown', handleKeyDown);
-//     }
-
-//     // Видаляємо обробник події при демонтуванні або зміні стану
-//     return () => {
-//       document.removeEventListener('keydown', handleKeyDown);
-//     };
-//   }, [isOpen, closeMenu]);
-
-//   return (
-//     <>
-//       <Overlay $isOpen={isOpen} onClick={closeMenu} />
-
-//       <MobileMenuContainer $isOpen={isOpen}>
-//         <CloseButton onClick={closeMenu}>
-//           <CloseIcon width={32} height={32} iconName="close" />
-//         </CloseButton>
-
-//         <MenuWrapper>
-//           <Nav closeMenu={closeMenu} />
-
-//           {isAuthenticated ? (
-//             <AuthNav closeMenu={closeMenu} />
-//           ) : (
-//             <UserNav
-//               closeMenu={closeMenu}
-//               userName={userName}
-//               userAvatar={userAvatar}
-//               onLogout={onLogout}
-//             />
-//           )}
-//         </MenuWrapper>
-//       </MobileMenuContainer>
-//     </>
-//   );
-// };
