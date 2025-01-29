fdf0bc839b3499cf987d954a49ef40c3935eaaf5
Yuriy <>
2025-01-29 17:59:15 +0200

    Update GlobalStyles.ts

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import { Reset } from './Reset';
-import { Variables } from './variables';
+import { Variables } from './Variables';
 import { Fonts } from './Fonts';
 
 export const GlobalStyles = createGlobalStyle`
