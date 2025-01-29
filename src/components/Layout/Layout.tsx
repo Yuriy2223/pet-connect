bf8f26df16092906ba1445530cf58150e4811b25
Yuriy <>
2025-01-29 18:40:36 +0200

    update App.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { ThemeType } from '../../styles/Theme';
 import { Header } from '../Header/Header';
+import { Outlet } from 'react-router-dom';
 
 interface LayoutProps {
-  children: React.ReactNode;
+  children?: React.ReactNode; // Робимо children необов'язковим
   toggleTheme: (theme: ThemeType) => void;
 }
 
-export const Layout: React.FC<LayoutProps> = ({
-  children,
-}) => {
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div>
       <Header />
-      <main>{children}</main>
+      <main>{children || <Outlet />}</main>
     </div>
   );
 };
