67c18879d8f986572b8d1e30ae531bfde2270057
Yuriy <>
2025-01-29 17:56:25 +0200

    update global styles

---
+++ src/styles/Reset.ts
@@ -0,0 +1,53 @@
+import { css } from 'styled-components';
+
+export const Reset = css`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    font-family: var(--font-family);
+    background-color: var(--background-body);
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: normal;
+  }
+
+  ul,
+  ol {
+    list-style: none;
+  }
+
+  a {
+    font-family: inherit;
+    text-decoration: none;
+    transition: all 300ms ease;
+  }
+
+  img {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
+
+  button {
+    font-family: inherit;
+    transition: all 300ms ease;
+    cursor: pointer;
+  }
+
+  input,
+  textarea {
+    font-family: inherit;
+    outline: none;
+    transition: all 300ms ease;
+  }
+`;
