b308ff80bd805b6f6ea559a06030c05fdf97b70a
Yuriy <>
2025-02-05 03:35:17 +0200

    1

--- src/redux/modal/selectors.ts
+++ src/redux/modal/selectors.ts
@@ -2,6 +2,8 @@ import { RootState } from '../store';
 
 export const selectIsModalOpen = (state: RootState) => state.modal.isOpen;
 
+export const selectIsModalClose = (state: RootState) => !state.modal.isOpen;
+
 export const selectModalType = (state: RootState) => state.modal.modalType;
 
 export const selectModalProps = (state: RootState) => state.modal.modalProps;
