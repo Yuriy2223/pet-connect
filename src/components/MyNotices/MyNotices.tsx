1189b7f590ffad36ca3645d95765e0b1bc3dee67
Yuriy <>
2025-02-09 23:49:39 +0200

    1

--- src/components/MyNotices/MyNotices.tsx
+++ src/components/MyNotices/MyNotices.tsx
@@ -1,31 +1,35 @@
 import { NavLink } from 'react-router-dom';
+import {
+  MyNoticesContainer,
+  MyNoticesFavorite,
+  MyNoticesInner,
+  MyNoticesViewed,
+} from './MyNotices.styled';
 
 export const MyNotices: React.FC<{
   activeTab: string;
   setActiveTab: (tab: string) => void;
 }> = ({ activeTab, setActiveTab }) => (
-  <PetsInformationWrappper>
-    <div>
-      <button
-        type="button"
+  <MyNoticesContainer>
+    <MyNoticesInner>
+      <MyNoticesFavorite
         onClick={() => setActiveTab('favorites')}
         className={activeTab === 'favorites' ? 'active' : ''}
       >
         My favorite pets
-      </button>
-      <button
-        type="button"
+      </MyNoticesFavorite>
+      <MyNoticesViewed
         onClick={() => setActiveTab('viewed')}
         className={activeTab === 'viewed' ? 'active' : ''}
       >
         Viewed
-      </button>
-    </div>
+      </MyNoticesViewed>
+    </MyNoticesInner>
     <p>
       Oops,
       <NavLink to="/friends">looks like there aren't any furries</NavLink> on
       our adorable page yet. Do not worry! View your pets on the "find your
       favorite pet" page and add them to your favorites.
     </p>
-  </PetsInformationWrappper>
+  </MyNoticesContainer>
 );
