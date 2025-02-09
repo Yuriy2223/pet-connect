b442711c23a7e85ccd0fddb9d583283d8678cc2d
Yuriy <>
2025-02-09 23:40:43 +0200

    update edit btn

--- src/components/EditUserButton.tsx
+++ src/components/EditUserButton.tsx
@@ -1,9 +1,62 @@
+import React, { useCallback } from 'react';
 import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import { openModal } from '../redux/modal/slice';
 
-export const EditUserButton: React.FC<{ onClick: () => void }> = ({
-  onClick,
-}) => (
-  <button onClick={onClick} type="button">
-    ✏️
-  </button>
-);
+const EditUserButtonContainer = styled.div`
+  display: none;
+
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const EditUserBtn = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 30px;
+  width: 178px;
+  height: 42px;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 1.29;
+  letter-spacing: -0.03em;
+  text-transform: uppercase;
+
+  color: ${({ theme }) => theme.primaryDark};
+  background-color: ${({ theme }) => theme.lightYellow};
+  border: 1px solid ${({ theme }) => theme.primaryDark};
+
+  &:hover,
+  &.active {
+    border: 1px solid ${({ theme }) => theme.primaryDark};
+    background-color: ${({ theme }) => theme.primaryDark};
+    color: ${({ theme }) => theme.white};
+  }
+`;
+
+export interface EditUserButtonProps {
+  onLogout?: () => void;
+  closeMenu?: () => void;
+}
+
+export const EditUserButton: React.FC<EditUserButtonProps> = () => {
+  const dispatch = useDispatch();
+
+  const handleLogoutClick = useCallback(() => {
+    dispatch(
+      openModal({
+        type: 'ModalEditUser',
+        props: { actionType: '???????????????????????????????????' },
+      })
+    );
+  }, [dispatch]);
+
+  return (
+    <EditUserButtonContainer>
+      <EditUserBtn onClick={handleLogoutClick}>Log Out</EditUserBtn>
+    </EditUserButtonContainer>
+  );
+};
