e262079556a5f6f07f22d1922cbd1dac228424f8
Yuriy <>
2025-03-03 22:15:03 +0200

    Update newsApi.ts

--- src/services/newsApi.ts
+++ src/services/newsApi.ts
@@ -1,17 +1,3 @@
-// import { GetNewsResponse } from '../App.types';
-// import { instance } from './Api';
-
-// export const fetchNewsApi = async (
-//   page: number,
-//   keyword: string,
-//   perPage: number
-// ): Promise<GetNewsResponse> => {
-//   const response = await instance.get<GetNewsResponse>('/api/news/', {
-//     params: { page, keyword, perPage },
-//   });
-//   return response.data;
-// };
-
 import { GetNewsResponse } from '../App.types';
 import { publicInstance } from './Api';
 
