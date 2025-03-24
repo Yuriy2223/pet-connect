e8daf2f2d7a3c25371e0d47e54d38f132503b232
Yuriy <>
2025-03-25 01:39:29 +0200

    update modal edit user

--- src/modals/ModalEditUser/ModalEditUser.styled.ts
+++ src/modals/ModalEditUser/ModalEditUser.styled.ts
@@ -1,28 +1,13 @@
 import styled from 'styled-components';
 import { Iconsvg } from '../../components/Common/Icons';
-import { FocusedProps } from './ModalEditUser';
-
-export const ErrorText = styled.p.withConfig({
-  shouldForwardProp: prop => !['isValid', 'isFieldFocused'].includes(prop),
-})<FocusedProps & { isValid?: boolean }>`
-  color: ${props => (props.isValid ? props.theme.green : props.theme.red)};
-  font-weight: 500;
-  font-size: 10px;
-  position: absolute;
-  bottom: -15px;
-  left: 10px;
-  opacity: ${props => (props.isFieldFocused ? 0 : 1)};
-  transition: opacity 300ms ease;
-`;
 
 export const ModalEditUserContainer = styled.div`
+  width: 100%;
   padding: 50px 20px 40px;
+  border-radius: 30px;
+  background: ${({ theme }) => theme.white};
   display: flex;
   flex-direction: column;
-  background: #fff;
-  border-radius: 30px;
-  width: 335px;
-  height: 473px;
 
   h2 {
     font-weight: 700;
@@ -37,32 +22,14 @@ export const ModalEditUserContainer = styled.div`
       line-height: 1.17;
     }
   }
-
   @media (min-width: 768px) {
-  }
-
-  @media (min-width: 1280px) {
+    padding: 50px 50px 40px;
   }
 `;
-export const EditFormWraper = styled.form`
+export const EditForm = styled.form`
   display: flex;
   flex-direction: column;
-  width: 295px;
-
-  @media (min-width: 320px) and (max-width: 374px) {
-    width: 100%;
-    height: 100%;
-  }
-  @media (min-width: 768px) {
-    width: 424px;
-    height: 506px;
-  }
-  @media (min-width: 1280px) {
-    width: 424px;
-    height: 516px;
-  }
 `;
-
 export const UploadWrapperAvatar = styled.div`
   display: flex;
   align-items: center;
@@ -72,10 +39,12 @@ export const UploadWrapperAvatar = styled.div`
   img {
     width: 80px;
     height: 80px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
     border-radius: 50%;
+
+    @media (min-width: 768px) {
+      width: 90px;
+      height: 90px;
+    }
   }
 
   div {
@@ -86,6 +55,11 @@ export const UploadWrapperAvatar = styled.div`
     align-items: center;
     justify-content: center;
     border-radius: 50%;
+
+    @media (min-width: 768px) {
+      width: 90px;
+      height: 90px;
+    }
   }
 `;
 export const IconAvatar = styled(Iconsvg)`
@@ -93,75 +67,48 @@ export const IconAvatar = styled(Iconsvg)`
   height: 40px;
   fill: ${({ theme }) => theme.primaryDark};
 `;
-export const UploadWrapperInputEndBtn = styled.div`
+export const UploadWrapperInputEndBtn = styled.div<{ $hasError?: boolean }>`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: 295px;
-  height: 42px;
-  margin-bottom: 10px;
+  height: 40px;
+  width: 100%;
+  margin-bottom: 18px;
 
   input {
-    padding: 8px;
-    margin: 5px 0;
-    border-radius: 4px;
-    border: 1px solid #f6b83d;
-    border-radius: 30px;
-    padding: 12px;
     width: 160px;
+    padding: 12px;
+    height: 40px;
+    border-radius: 30px;
+    border: 1px solid
+      ${({ theme, $hasError }) => ($hasError ? theme.red : theme.primaryDark)};
+
+    @media (min-width: 768px) {
+      width: 220px;
+    }
   }
-`;
-export const IconUploadBtnAvatar = styled(Iconsvg)`
-  width: 18px;
-  height: 18px;
-  stroke: ${({ theme }) => theme.primaryDark};
-`;
-export const InputStyled = styled.input`
-  width: 100%;
-  border: 1px solid #f6b83d;
-  border-radius: 30px;
-  padding: 12px;
-  height: 42px;
-  font-family: inherit;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 129%;
-  letter-spacing: -0.03em;
-  color: #262626;
-`;
-
-export const WrapperInputsBlok = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  /* gap: 10px; */
-  gap: 20px;
-  margin-bottom: 20px;
-`;
-
-export const LabelInput = styled.label`
-  position: relative;
-`;
-export const ErrorMessage = styled.p`
-  color: red;
-  font-size: 12px;
 `;
 export const UploadButtonAvatar = styled.label`
   display: flex;
   align-items: center;
   justify-content: space-between;
   border-radius: 30px;
-  padding: 12px;
+  padding: 8px;
   width: 126px;
-  height: 100%;
-  background: ${({ theme }) => theme.lightYellow};
+  height: 40px;
   font-weight: 500;
   font-size: 12px;
   line-height: 1.33;
   letter-spacing: -0.02em;
   color: ${({ theme }) => theme.black};
-  border: none;
+  background-color: ${({ theme }) => theme.lightYellow};
+  border: 1px solid ${({ theme }) => theme.primaryDark};
   cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.primaryDark};
+    color: ${({ theme }) => theme.white};
+  }
 
   @media (max-width: 374px) {
     width: 117px;
@@ -169,42 +116,90 @@ export const UploadButtonAvatar = styled.label`
 
   @media (min-width: 768px) {
     width: 146px;
-    height: 42px;
-  }
-
-  &:hover {
-    background-color: ${({ theme }) => theme.primaryDark};
-    color: ${({ theme }) => theme.white};
+    padding: 16px;
   }
 `;
-export const Button = styled.button`
-  border-radius: 30px;
+export const IconUploadBtnAvatar = styled(Iconsvg)`
+  width: 18px;
+  height: 18px;
+  stroke: ${({ theme }) => theme.primaryDark};
+`;
+export const WrapperInputsBlok = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+  margin-bottom: 20px;
+
+  @media (min-width: 768px) {
+    margin-bottom: 30px;
+  }
+`;
+export const LabelInput = styled.label`
+  position: relative;
+`;
+export const InputStyled = styled.input.withConfig({
+  shouldForwardProp: prop => !['hasError', 'isFocused'].includes(prop),
+})<{
+  hasError?: boolean;
+  isFocused?: boolean;
+}>`
   width: 100%;
-  height: 42px;
+  border: 1px solid
+    ${({ theme, hasError, isFocused }) =>
+      isFocused ? theme.primaryDark : hasError ? theme.red : theme.primaryDark};
+  border-radius: 30px;
+  padding: 12px;
+  height: 40px;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 1.29;
+  letter-spacing: -0.03em;
+  color: ${({ theme }) => theme.black};
+
+  &:hover,
+  &:focus {
+    border-color: ${({ theme, hasError, isFocused }) =>
+      isFocused ? theme.green : hasError ? theme.red : theme.green};
+  }
+`;
+export const ErrorMessage = styled.p<{
+  $focus?: boolean;
+}>`
+  color: ${({ theme }) => theme.red};
+  font-weight: 500;
+  font-size: 10px;
+  position: absolute;
+  bottom: -15px;
+  left: 10px;
+  opacity: ${({ $focus }) => ($focus ? 0 : 1)};
+  transition: opacity 300ms ease;
+`;
+export const SubmitButton = styled.button`
+  border-radius: 30px;
+  width: 80%;
+  margin: 0 auto;
+  height: 40px;
   font-weight: 700;
   font-size: 14px;
   line-height: 1.29;
   letter-spacing: -0.03em;
-  border: none;
   display: flex;
   align-items: center;
   justify-content: center;
-  color: ${({ theme }) => theme.white};
-  background-color: ${({ theme }) => theme.primaryLight};
+  color: ${({ theme }) => theme.primaryDark};
+  background-color: ${({ theme }) => theme.lightYellow};
+  border: 1px solid ${({ theme }) => theme.primaryDark};
 
   &:hover,
   &:active {
     background-color: ${({ theme }) => theme.primaryDark};
+    color: ${({ theme }) => theme.white};
   }
 
-  /* @media (min-width: 768px) {
-    width: 200px;
+  @media (min-width: 768px) {
+    width: 75%;
     height: 48px;
     font-size: 16px;
     line-height: 1.25;
-  } */
-
-  &:disabled {
-    background: #ccc;
   }
 `;
