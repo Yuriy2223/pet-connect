8e846125d4fbd7b170466a7bab8f860f7159c05d
Yuriy <>
2025-03-25 18:09:40 +0200

    Update RestrictedRoute.tsx

--- src/routes/RestrictedRoute.tsx
+++ src/routes/RestrictedRoute.tsx
@@ -6,6 +6,19 @@ import {
   selectIsAuthChecking,
 } from '../redux/auth/selectors';
 
+// export const RestrictedRoute = () => {
+//   const isSignedIn = useSelector(selectIsSignedIn);
+//   const isAuthLoading = useSelector(selectAuthLoading);
+//   const isAuthChecking = useSelector(selectIsAuthChecking);
+//   const location = useLocation();
+
+//   if (isAuthLoading || isAuthChecking) return null;
+
+//   const redirectTo = location.pathname === '/register' ? '/login' : '/profile';
+
+//   return isSignedIn ? <Navigate to={redirectTo} replace /> : <Outlet />;
+// };
+
 export const RestrictedRoute = () => {
   const isSignedIn = useSelector(selectIsSignedIn);
   const isAuthLoading = useSelector(selectAuthLoading);
@@ -14,7 +27,9 @@ export const RestrictedRoute = () => {
 
   if (isAuthLoading || isAuthChecking) return null;
 
-  const redirectTo = location.pathname === '/register' ? '/login' : '/profile';
+  if (isSignedIn && location.pathname === '/register') {
+    return <Navigate to="/login" replace />;
+  }
 
-  return isSignedIn ? <Navigate to={redirectTo} replace /> : <Outlet />;
+  return isSignedIn ? <Navigate to="/profile" replace /> : <Outlet />;
 };
